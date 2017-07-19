9c7d222a35bcb49453c8e254657ef42311602c25
Felix Langebach <felixlangebach@gmail.com>
2017-07-20 00:23:19 +0200

    Add locations

--- src/data/locations.js
+++ src/data/locations.js
@@ -16,8 +16,8 @@ const database = {
                 shortDescription: "Kurzer Informationstext kommt hier hin"
             },
             // Strongline ausserhalb der Stadt
-            "agglo-st-fiden": {
-                company: "Strongline Agglo SG",
+            "st-fiden": {
+                company: "Strongline St. Fiden SG",
                 query: "st-gallen",
                 contact: {
                     chief: "Herr Muster",
@@ -33,12 +33,324 @@ const database = {
         title: "Zürich",
         list: {
             "city-kreis-1": {
-                company: "Strongline ZH",
+                company: "Strongline ZH Kreis 1",
                 query: "zurich",
                 contact: {
                     chief: "Herr Muster",
                     address: "Musterstrasse 33",
-                    location: "Zürich, Schweiz"
+                    location: "Zürich Kreis 1, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "city-kreis-2": {
+                company: "Strongline ZH Kreis 2",
+                query: "zurich",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Zürich Kreis 2, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "city-kreis-3": {
+                company: "Strongline ZH Kreis 3",
+                query: "zurich",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Zürich Kreis 3, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    dietikon: {
+        title: "Dietikon",
+        list: {
+            "dietikon-stadt": {
+                company: "Strongline Dietikon",
+                query: "dietikon",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Dietikon, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    genf: {
+        title: "Genf",
+        list: {
+            "genf-zentrum": {
+                company: "Strongline Genf",
+                query: "genf",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Genf, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "genf-see": {
+                company: "Strongline Genf See",
+                query: "genf",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Genf See, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "genf-west": {
+                company: "Strongline Genf West",
+                query: "genf",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Genf, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    chur: {
+        title: "Chur",
+        list: {
+            "chur-zentrum": {
+                company: "Strongline Chur",
+                query: "chur",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Chur, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "chur-west": {
+                company: "Strongline Chur West",
+                query: "chur",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Chur West, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    "st-moritz": {
+        title: "St. Moritz",
+        list: {
+            "st-moritz-zentrum": {
+                company: "Strongline Chur",
+                query: "st-moritz",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "St. Moritz, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    arosa: {
+        title: "Arosa",
+        list: {
+            "arosa-zentrum": {
+                company: "Strongline Arosa",
+                query: "arosa",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Arosa, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    kreuzlingen: {
+        title: "Kreuzlingen",
+        list: {
+            "kreuzlingen-zentrum": {
+                company: "Strongline Kreuzlingen",
+                query: "kreuzlingen",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Kreuzlingn, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    wil: {
+        title: "Wil",
+        list: {
+            "wil-zentrum": {
+                company: "Strongline Wil",
+                query: "wil",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Wil, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    andermatt: {
+        title: "Andermatt",
+        list: {
+            "andermatt-zentrum": {
+                company: "Strongline Andermatt",
+                query: "andermatt",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Andermatt, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    frauenfeld: {
+        title: "Frauenfeld",
+        list: {
+            "frauenfeld-zentrum": {
+                company: "Strongline Frauenfeld",
+                query: "frauenfeld",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Frauenfeld, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    bern: {
+        title: "Bern",
+        list: {
+            "bern-zentrum": {
+                company: "Strongline Bern",
+                query: "bern",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Bern, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "bern-west": {
+                company: "Strongline Bern West",
+                query: "bern",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Bern, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "bern-ost": {
+                company: "Strongline Bern Ost",
+                query: "bern",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Bern, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    pfaeffikon: {
+        title: "Pfäffikon",
+        list: {
+            "pfaeffikon-zentrum": {
+                company: "Strongline Pfäffikon",
+                query: "pfaeffikon",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Pfäffikon, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            },
+            "pfaeffikon-west": {
+                company: "Strongline Pfäffikon West",
+                query: "pfaeffikon",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Pfäffikon, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    richterswil: {
+        title: "Richterswil",
+        list: {
+            "richterswil-zentrum": {
+                company: "Strongline Richterswil",
+                query: "richterswil",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Richterswil, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    dietlikon: {
+        title: "Dietlikon",
+        list: {
+            "dietlikon-zentrum": {
+                company: "Strongline Dietlikon",
+                query: "dietlikon",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Dietlikon, Schweiz"
+                },
+                description: "Informationstext kommt hier hin",
+                shortDescription: "Kurzer Informationstext kommt hier hin"
+            }
+        }
+    },
+    laax: {
+        title: "Laax",
+        list: {
+            "laax-zentrum": {
+                company: "Strongline Laax",
+                query: "laax",
+                contact: {
+                    chief: "Herr Muster",
+                    address: "Musterstrasse 33",
+                    location: "Laax, Schweiz"
                 },
                 description: "Informationstext kommt hier hin",
                 shortDescription: "Kurzer Informationstext kommt hier hin"
